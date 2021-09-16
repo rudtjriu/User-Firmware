ebba2925ae5f0e868cb54d7db7e801853c113155
rudtjriu@naver.com <rudtjriu@naver.com>
2021-09-16 10:52:45 +0900

    commit message

---
+++ education/test1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace education
+{
+    /// <summary>
+    /// test1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class test1 : Window
+    {
+        public test1()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string value = 과목.Text;
+
+            if( value == "수학" )
+            {
+                education.수학 section1 = new education.수학();
+
+                section1.ShowDialog();
+            }
+            else if(value == "과학")
+            {
+                education.과학 section2 = new education.과학();
+
+                section2.ShowDialog();
+
+            }
+        }
+    }
+    
+}
---
+++ education/수학.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace education
+{
+    /// <summary>
+    /// test2.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class 수학 : Window
+    {
+        public 수학()
+        {
+            InitializeComponent();
+        }
+    }
+}
